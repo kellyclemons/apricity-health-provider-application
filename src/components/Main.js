cb985b6188e2a5eb6fc5f62cca6d76fa713124df
Kelly Clemons <kelly.clemons@mac.com>
2017-12-19 00:52:23 -0600

    adding more example charts

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,9 @@ import Navbar from './Navbar.js';
 import GenInfo from './GenInfo.js';
 // import {Patient} from './Patient.js';
 import {Graph} from './GraphType.js';
-import HeartRateBarChart from './HeartRateBarChart.js';
-import HeartRateLineChart from './HeartRateLineChart.js';
+import BloodPressureBarChart from './BloodPressureTest.js';
+// import HeartRateBarChart from './HeartRateBarChart.js';
+// import HeartRateLineChart from './HeartRateLineChart.js';
 // import BarChart from './TestBarChart.js';
 // import LineChart from './TestLineChart.js';
 
@@ -16,11 +17,12 @@ export default class Main extends React.Component {
           {/* <div className="main-title">Patient Monitoring</div> */}
           <GenInfo />
           <div>
-            <HeartRateBarChart />
-            <HeartRateLineChart />
+            {/* <HeartRateBarChart /> */}
+            {/* <HeartRateLineChart /> */}
             {/* <Patient /> */}
             {/* <BarChart /> */}
             {/* <LineChart /> */}
+            <BloodPressureBarChart data={[5,10,1,3]} size={[500,500]} />
             <Graph />
             <Graph />
             <Graph />
