31a9a4049e2aade100d678d9d81d77a19ba3c263
Kelly Clemons <kelly.clemons@mac.com>
2017-12-19 17:20:45 -0600

    everything works, need to add working interactive chart to body

--- components/ethereum/components/Main.js
+++ components/ethereum/components/Main.js
@@ -3,12 +3,8 @@ import Navbar from './Navbar.js';
 import GenInfo from './GenInfo.js';
 // import {Patient} from './Patient.js';
 import {Graph} from './GraphType.js';
-// import BloodPressureBarChart from './BloodPressureTest.js';
-// import HeartRateBarChart from './HeartRateBarChart.js';
-// import HeartRateLineChart from './HeartRateLineChart.js';
 import PatientHeartRateChart from './PatientHeartRate.js';
-// import BarChart from './TestBarChart.js';
-// import LineChart from './TestLineChart.js';
+
 
 export default class Main extends React.Component {
   render() {
@@ -18,13 +14,6 @@ export default class Main extends React.Component {
           {/* <div className="main-title">Patient Monitoring</div> */}
           <GenInfo />
           <div>
-            {/* <HeartRateBarChart /> */}
-            {/* <HeartRateLineChart /> */}
-            <PatientHeartRateChart />
-            {/* <Patient /> */}
-            {/* <BarChart /> */}
-            {/* <LineChart /> */}
-            {/* <BloodPressureBarChart data={[5,10,1,3]} size={[500,500]} /> */}
             <Graph />
             <Graph />
             <Graph />
--- src/components/InteractiveLineChart.js
+++ src/components/InteractiveLineChart.js
@@ -1,75 +1,69 @@
-// import React from 'react';
-//
-// // Similar to bitcoin.js file
-//
-// import { withScreenSize } from '@vx/responsive';
-// import Background from '../../components/background.js';
-// import HeartrateValue from '../../components/heartratevalue';
-// // import BitcoinPrice from '../../components/bitcoinprice';
-// import jsonData from '../../components/heartratedata.json';
-//
-//
-// console.log(jsonData);
-// class InteractiveTest extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: {}
-//     };
-//   }
-//
-//   //https://hl7.org/fhir/2017Jan/observation-example-heart-rate.json
-//   componentDidMount() {
-//     // fetch('https://hl7.org/fhir/2017Jan/observation-example-heart-rate.json')
-//
-//         this.setState({
-//           data: jsonData
-//         });
-//
-//   }
-//   render() {
-//     const { screenWidth, screenHeight } = this.props;
-//     const { data } = this.state;
-//     return (
-//       <div className="app">
-//         <Background width={screenWidth} height={screenHeight} />
-//         <div className="center">
-//           {/* CHANGE LINE BELOW */}
-//           <HeartrateValue data={data} width={screenWidth} height={screenHeight} />
-//           {/* <BitcoinPrice data={data} width={screenWidth} height={screenHeight} /> */}
-//           <p className="identity">
-//             CHANGE LINE BELOW
-//             {data.disclaimer}
-//           </p>
-//         </div>
-//         <style jsx>{`
-//           .app,
-//           .center {
-//             position: absolute;
-//             top: 0;
-//             left: 0;
-//             right: 0;
-//             bottom: 0;
-//             overflow: hidden;
-//             display: flex;
-//             font-family: arial;
-//             flex-direction: column;
-//           }
-//
-//           .identity {
-//             margin-top: 35px;
-//             font-size: 11px;
-//             color: white;
-//             opacity: 0.4;
-//           }
-//           .center {
-//             align-items: center;
-//             justify-content: center;
-//           }
-//         `}</style>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default withScreenSize(InteractiveTest);
+import React from 'react';
+
+import { withScreenSize } from '@vx/responsive';
+import Background from '../../components/background.js';
+import HeartrateValue from '../../components/heartratevalue';
+import jsonData from '../../components/heartratedata.json';
+
+
+console.log(jsonData);
+class InteractiveTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+        this.setState({
+          data: jsonData
+        });
+  }
+
+  render() {
+    const { screenWidth, screenHeight } = this.props;
+    const { data } = this.state;
+    return (
+      <div className="app">
+        <Background width={screenWidth} height={screenHeight} />
+        <div className="center">
+          {/* CHANGE LINE BELOW */}
+          <HeartrateValue data={data} width={screenWidth} height={screenHeight} />
+          {/* <BitcoinPrice data={data} width={screenWidth} height={screenHeight} /> */}
+          <p className="identity">
+            CHANGE LINE BELOW
+            {data.disclaimer}
+          </p>
+        </div>
+        <style jsx>{`
+          .app,
+          .center {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            overflow: hidden;
+            display: flex;
+            font-family: arial;
+            flex-direction: column;
+          }
+
+          .identity {
+            margin-top: 35px;
+            font-size: 11px;
+            color: white;
+            opacity: 0.4;
+          }
+          .center {
+            align-items: center;
+            justify-content: center;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default withScreenSize(InteractiveTest);
