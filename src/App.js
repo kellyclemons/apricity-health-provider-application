527e52015528c907c841e5bc1ded48c815547aae
Kelly Clemons <kelly.clemons@mac.com>
2017-12-18 23:17:34 -0600

    imported styles in app.js and rendered sidebar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import './styles/GenInfo.css'; // GENERAL INFO STYLES
+import './styles/GenResets.css'; // RESET AND COLOR VARIABLE STYLES
+import './styles/Main.css'; // MAIN MEDICAL STYLES
+import './styles/Navbar.css'; // NAVBAR STYLES
+import './styles/Patient.css'; // PATIENT STYLES
+import './styles/Provider.css'; // PROVIDER STYLES
+import './styles/Sidebar.css'; // SIDEBAR STYLES
+// import './styles/TestComponent.css'; // REMOVE LATER
+
+import Sidebar from './components/Sidebar.js';
+// import {MainMedical} from './components/main-medical.js';
+// import {Patient} from './components/patient.js'
+
 // import BarChart from './components/TestBarChart.js';
 // import LineChart from './components/TestLineChart.js';
 
@@ -17,7 +30,7 @@ class App extends Component {
         </div> */}
 
         <div className="App-container">
-
+          <Sidebar />
         </div>
 
       </div>
