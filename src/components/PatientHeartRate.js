8be1a1f17381ddc5a6a60870cbc8166b134d2c48
Kelly Clemons <kelly.clemons@mac.com>
2017-12-19 09:43:25 -0600

    patient heart rate line chart renders but needs modifications

--- src/components/PatientHeartRate.js
+++ src/components/PatientHeartRate.js
@@ -1,11 +1,78 @@
 import React from 'react';
+import data from "../data/patient-heart-rate.json";
+
+import { ResponsiveXYFrame } from "semiotic";
+import { scaleTime } from "d3-scale";
+import { AnnotationCalloutElbow } from "react-annotation";
+
+// LINE CHART
+
+const tooltipCreator = d => (
+  <div className="tooltip-content">
+    <p>{d.parentLine.bpmReading}</p>
+    <p>Patient Heart Rate {d.patientBPM}</p>
+    <p>Week {d.week}</p>
+  </div>
+);
 
 export default class PatientHeartRateChart extends React.Component {
   render() {
-    return(
-      <div>
-        TEST PATIENT HEART RATE CHART FINAL
+    const axes = [
+      {
+        orient: "left",
+        tickFormat: d => d / 10000000 + "m"
+      },
+      { orient: "bottom", tickFormat: d => d.getMonth() }
+    ];
+    const annotations = [
+      {
+        type: "y",
+        grossWeekly: 150000000,
+        label: "High Risk"
+      },
+      {
+        date: "2016-07-01",
+        title: "Finding Dory",
+        dx: 30,
+        dy: -50,
+        type: AnnotationCalloutElbow,
+        label: "Very important day"
+      }
+    ];
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        <ResponsiveXYFrame
+          size={[400, 200]}
+          // responsiveWidth={true}
+          lines={data.filter((d, i) => i < 50)}
+          lineStyle={d => ({
+            stroke: d.bpmReading.length > 10 ? "blue" : "red",
+            fill: d.bpmReading.length > 10 ? "blue" : "red",
+            strokeOpacity: 1,
+            fillOpacity: 0.25
+          })}
+          lineType={"line"}
+          lineIDAccessor={d => d.bpmReading}
+          xScaleType={scaleTime()}
+          xAccessor={d => new Date(d.date)}
+          yAccessor={"patientBPM"}
+          hoverAnnotation={true}
+          tooltipContent={tooltipCreator}
+          axes={axes}
+          annotations={annotations}
+        />
       </div>
     );
   }
 }
+
+
+// export default class PatientHeartRateChart extends React.Component {
+//   render() {
+//     return(
+//       <div>
+//
+//       </div>
+//     );
+//   }
+// }
