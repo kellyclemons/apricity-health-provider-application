066031700322be879d5d5de896e00591d18c897a
Kelly Clemons <kelly.clemons@mac.com>
2017-12-20 16:17:49 -0600

    fixed build error

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,11 +4,11 @@ export class ProgressBar extends React.Component {
   render() {
     return(
       <div className="gen-info-box">
-        {/* <div class="progress-circle" data-progress="10"></div> */}
-        <div class="progress-circle" data-progress="25"></div>
-        {/* <div class="progress-circle" data-progress="50"></div> */}
-        <div class="progress-circle" data-progress="75"></div>
-        {/* <div class="progress-circle" data-progress="100"></div> */}
+        {/* <div className="progress-circle" data-progress="10"></div> */}
+        <div className="progress-circle" data-progress="25"></div>
+        {/* <div className="progress-circle" data-progress="50"></div> */}
+        <div className="progress-circle" data-progress="75"></div>
+        {/* <div className="progress-circle" data-progress="100"></div> */}
       </div>
     );
   }
