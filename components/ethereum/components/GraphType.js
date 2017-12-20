f6fc87150a0c089b2010484a2f0270cbfef7cff7
Kelly Clemons <kelly.clemons@mac.com>
2017-12-20 16:16:43 -0600

    deleted test files

--- components/ethereum/components/GraphType.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class Graph extends React.Component {
-  render() {
-    return(
-      <div className="graph">GRAPH TEST</div>
-    );
-  }
-}
--- components/ethereum/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TopCorner } from './topcorner/TopCorner';
-export { default as Banner } from './banner/Banner';
-export { default as Chart } from './chart/Chart';
-export { default as Chart2 } from './chart/Chart2';
